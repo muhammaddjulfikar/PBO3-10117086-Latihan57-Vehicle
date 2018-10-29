9b6606c08109ff2fcb1a334fd3fcb420e1b7844a
djulfikar <muhammaddjulfikar7@gmail.com>
2018-10-29 23:12:08 +0700

    PBO3-10117086-Latihan57-Vehicle

---
+++ src/pbo3/pkg10117086/latihan57/vehicle/PBO310117086Latihan57Vehicle.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan57.vehicle;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA : RD Muhammad Djulfikar BU 
+ * KELAS : IF3 
+ * NIM : 10117086
+ * DESKRIPSI PROGRAM :
+ */
+public class PBO310117086Latihan57Vehicle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+   
+    }
+    
+}
